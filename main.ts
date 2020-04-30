2f3bf874f5509b1a73249d0ae7517f27dd53c7f4
KKITC-Frason <KKITC-Frason@users.noreply.github.com>
2020-04-30 18:01:34 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -214,4 +214,118 @@ namespace matrixmicro {
     return d * 0.017;
   }
 
+  namespace servos {
+      //% fixedInstances
+      export class Servo {
+          private _minAngle: number;
+          private _maxAngle: number;
+          private _stopOnNeutral: boolean;
+
+          constructor() {
+              this._minAngle = 0;
+              this._maxAngle = 180;
+              this._stopOnNeutral = false;
+          }
+
+          private clampDegrees(degrees: number): number {
+              degrees = degrees | 0;
+              degrees = Math.clamp(this._minAngle, this._maxAngle, degrees);
+              return degrees;
+          }
+
+          /**
+           * Set the servo angle
+           */
+          //% weight=100 help=servos/set-angle
+          //% blockId=servoservosetangle block="set %servo angle to %degrees=protractorPicker °"
+          //% degrees.defl=90
+          //% servo.fieldEditor="gridpicker"
+          //% servo.fieldOptions.width=220
+          //% servo.fieldOptions.columns=2
+          //% blockGap=8
+          //% parts=microservo trackArgs=0
+          //% group="Positional"
+          setAngle(degrees: number) {
+              degrees = this.clampDegrees(degrees);
+              this.internalSetAngle(degrees);
+          }
+
+          protected internalSetAngle(angle: number): void {
+
+          }
+
+          /**
+           * Gets the minimum angle for the servo
+           */
+          public get minAngle() {
+              return this._minAngle;
+          }
+
+          /**
+           * Gets the maximum angle for the servo
+           */
+          public get maxAngle() {
+              return this._maxAngle;
+          }
+
+          /**
+           * Set the possible rotation range angles for the servo between 0 and 180
+           * @param minAngle the minimum angle from 0 to 90
+           * @param maxAngle the maximum angle from 90 to 180
+           */
+          //% help=servos/set-range
+          //% blockId=servosetrange block="set %servo range from %minAngle to %maxAngle"
+          //% minAngle.min=0 minAngle.max=90
+          //% maxAngle.min=90 maxAngle.max=180 maxAngle.defl=180
+          //% servo.fieldEditor="gridpicker"
+          //% servo.fieldOptions.width=220
+          //% servo.fieldOptions.columns=2
+          //% parts=microservo trackArgs=0
+          //% group="Configuration"
+          //% blockGap=8
+          public setRange(minAngle: number, maxAngle: number) {
+              this._minAngle = Math.max(0, Math.min(90, minAngle | 0));
+              this._maxAngle = Math.max(90, Math.min(180, maxAngle | 0));
+          }
+
+          /**
+           * Set a servo stop mode so it will stop when the rotation angle is in the neutral position, 90 degrees.
+           * @param on true to enable this mode
+           */
+          //% help=servos/set-stop-on-neutral
+          //% blockId=servostoponneutral block="set %servo stop on neutral %enabled"
+          //% enabled.shadow=toggleOnOff
+          //% group="Configuration"
+          //% blockGap=8
+          //% servo.fieldEditor="gridpicker"
+          //% servo.fieldOptions.width=220
+          //% servo.fieldOptions.columns=2
+          public setStopOnNeutral(enabled: boolean) {
+              this._stopOnNeutral = enabled;
+          }
+
+          protected internalStop() { }
+      }
+
+      export class PinServo extends Servo {
+          private _pin: PwmOnlyPin;
+
+          constructor(pin: PwmOnlyPin) {
+              super();
+              this._pin = pin;
+          }
+
+          protected internalSetAngle(angle: number): void {
+              this._pin.servoWrite(angle);
+          }
+
+          protected internalSetPulse(micros: number): void {
+              this._pin.servoSetPulse(micros);
+          }
+
+          protected internalStop() {
+              this._pin.digitalWrite(false);
+          }
+      }
+  }
 }
