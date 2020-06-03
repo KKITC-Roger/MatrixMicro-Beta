ecf052bf7a7392377b169a7ed3fda94d2233621b
KKITC-Frason <KKITC-Frason@users.noreply.github.com>
2020-06-03 13:47:49 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -198,7 +198,7 @@ namespace matrixmicro {
 		//% block="RGB2"
 		S2 = 2,
 	}
-  //%block="RGB Led at port %seport R %number|G %number|B %number"
+  //%block="RGB Led at port %seport R %number G %number B %number"
   export function rgb_led(seport: Led_port = 1, r: number = 0, g:number = 0, b: number = 0): void {
     if (r > 100)r = 100
     if (r < 0)r = 0
